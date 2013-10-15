8fbf14907f449925f3574e133cd8f7fb105cbcb4
Nathan Hopkins <natehop@gmail.com>
2013-10-15 15:57:41 -0600

    Added polish to the example app.

--- example/run.rb
+++ example/run.rb
@@ -1,6 +1,11 @@
 # An example of how to leverage the Goldmine Ruby GEM.
 # Extracts specific info about WIFI hotspots in NY.
 # Data source: https://nycopendata.socrata.com/data
+#
+# Generate the index.html page:
+#
+#   ruby /path/to/project/example/run.rb
+#
 
 require "csv"
 require "erb"
@@ -34,9 +39,76 @@ mined_data = mined_data.pivot(:library) do |row|
 end
 
 # -----------------------------------------------------------------------------
-# 3. define a simple presenter for rendering
+# 3. define a presenter to help with rendering
 # -----------------------------------------------------------------------------
-class Presenter < Struct.new(:mined_data)
+class Presenter < Struct.new(:data, :mined_data)
+  include ActiveSupport::NumberHelper
+
+  def columns
+    [:city, :free, :library, :hotspots, :percentage, :address_sum, :location_names, :zip_codes]
+  end
+
+  def header_value(column)
+    case column
+    when :address_sum
+      %Q{<span
+        data-title="Computed Column"
+        data-content="The sum of all the address numbers in the set of matching records. Note: This is a contrived example illustrating the ability to compute column values."
+        data-toggle="popover"
+        data-placement="top">
+        #{column.to_s.humanize}
+      </span>}
+    when :location_names
+      %Q{<span
+        data-title="Computed Column"
+        data-content="The total number of location names in the set of matching records. e.g. Starbucks, Barnes &amp; Noble, etc..."
+        data-toggle="popover"
+        data-placement="bottom">
+        #{column.to_s.humanize}
+      </span>}
+    when :zip_codes
+      %Q{<span
+        data-title="Computed Column"
+        data-content="The total number of zip codes in the set of matching records."
+        data-toggle="popover"
+        data-placement="top">
+        #{column.to_s.humanize}
+      </span>}
+    else
+      column.to_s.humanize
+    end
+  end
+
+  def cell_value(column, record)
+    case column
+    when :city
+      record.first[column]
+    when :free, :library
+      if record.first[column]
+        "<span class='glyphicon glyphicon-star'></span>"
+      else
+        nil
+      end
+    when :hotspots
+      record.last.length
+    when :percentage
+      number_to_percentage (record.last.length / data.length.to_f) * 100, :precision => 2
+    when :address_sum
+      record.last.reduce(0) do |memo, row|
+        memo + row["ADDRESS"].to_s.gsub(/\D/, "").to_i
+      end
+    when :location_names
+      record.last.map { |row| row["NAME"].to_s.strip }.uniq.length
+    when :zip_codes
+      record.last.map { |row| row["ZIP"].to_s.strip }.uniq.length
+    else
+      nil
+    end
+  end
+
+  def paid
+    mined_data.select { |key, val| !key[:free] }
+  end
 
   def free_libraries
     mined_data.select { |key, val| key[:free] && key[:library] }
@@ -62,6 +134,6 @@ end
 file_path = File.expand_path("../index.html", __FILE__)
 File.open(file_path, "w") do |file|
   template = File.read(File.expand_path("../index.html.erb", __FILE__))
-  file.write Presenter.new(mined_data).render(template)
+  file.write Presenter.new(data, mined_data).render(template)
 end
 
