4f37e3cc0d619246339ea0bd89eb329972e23023
Nathan Hopkins <natehop@gmail.com>
2015-05-30 15:09:01 -0600

    Added convenience methods for rolling up sums/counts.

--- lib/goldmine.rb
+++ lib/goldmine.rb
@@ -3,9 +3,21 @@ require "array_miner"
 require "hash_miner"
 
 module Goldmine
-  def self.miner(object)
-    return ArrayMiner.new(object) if object.is_a?(Array)
-    return HashMiner.new(object) if object.is_a?(Hash)
-    nil
+  class << self
+    def miner(object)
+      return ArrayMiner.new(object) if object.is_a?(Array)
+      return HashMiner.new(object) if object.is_a?(Hash)
+      nil
+    end
+
+    def sum_rows_on(column, rows)
+      rows.reduce(0) { |sum, row| sum += row[column] }
+    end
+
+    def sum_pivoted_on(column, pivoted)
+      pivoted.each_with_object({}) do |keypair, memo|
+        memo[keypair.first] = sum_rows_on(column, keypair.last)
+      end
+    end
   end
 end
